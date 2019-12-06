bfb2e3765d2614e908c95bb26b9307415ee6a55d
getinthedamnbox <getinthedamnbox@gmail.com>
2019-12-06 15:59:16 -0500

    Added db-config.js

---
+++ db-config.js
@@ -0,0 +1,8 @@
+// Secret. Do not push to repository.
+module.exports = {
+  user: 'admin',
+  password: 'AdminPass123',
+  host: 'cis550-project.ctzxic9gxjzj.us-east-1.rds.amazonaws.com',
+  port: 1521,
+  database: 'information',
+};
